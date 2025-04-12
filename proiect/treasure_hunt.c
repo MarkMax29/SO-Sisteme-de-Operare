60dc52cd060fe198773399a2c1b91dd89b970d1b
MarkMax29 <horvath29.m.mark@gmail.com>
2025-04-12 23:37:02 +0300

    version 2- cu remove_hunt si list

--- proiect/treasure_hunt.c
+++ proiect/treasure_hunt.c
@@ -9,7 +9,7 @@
 #include<time.h>
 #include<errno.h>
 #include<unistd.h>
-
+#include<stddef.h>
 
 typedef struct TREASURE{
     int id;
@@ -19,8 +19,23 @@ typedef struct TREASURE{
     int value;
 }TREASURE;
 
+void read_line(char * buffer,size_t size)//functie generala de citire cu read
+{
+    ssize_t len=read(0,buffer,size-1);
+    if(len>0)
+    {
+        if(buffer[len-1]=='\n')
+            buffer[len-1]='\0';
+        
+    }
+    else
+    {
+        buffer[0]='\0';
+    }
+}
 TREASURE *creare_comoare()
 {
+    char temp[256];//un buffer temporar
     TREASURE *comoara=NULL;
     comoara=malloc(sizeof(TREASURE));
     if(comoara==NULL)
@@ -29,32 +44,40 @@ TREASURE *creare_comoare()
         exit(-1);
     }
     printf("ID: ");
-    scanf("%d",&comoara->id);
+    fflush(stdout);
+    read_line(temp,sizeof(temp));
+    comoara->id=atoi(temp);
 
-    getchar();
     printf("User: ");
-    fgets(comoara->user,34,stdin);
-    comoara->user[strlen(comoara->user)-1]='\0';
+    fflush(stdout);
+    read_line(comoara->user,sizeof(comoara->user));
 
     printf("Longitude: ");
-    scanf("%f",&comoara->longitude);
+    fflush(stdout);
+    read_line(temp,sizeof(temp));
+    comoara->longitude=strtof(temp,NULL);//conversie string
 
     printf("Latitude: ");
-    scanf("%f",&comoara->latitude);
+    fflush(stdout);
+    read_line(temp,sizeof(temp));
+    comoara->latitude=strtof(temp,NULL);
 
-    getchar();
+    
     printf("Clue: ");
-    fgets(comoara->clue,79,stdin);
-    comoara->clue[strlen(comoara->clue)-1]='\0';
+    fflush(stdout);
+    read_line(comoara->clue,sizeof(comoara->clue));
 
     printf("Value: ");
-    scanf("%d",&comoara->value);
+    fflush(stdout);
+    read_line(temp,sizeof(temp));
+    comoara->value=atoi(temp);
 
     return comoara;
 
 }
 
-void loggin_action(const char *action,char *path)
+void loggin_action(const char *action,char *path)//functie care salveaza sau creaza(in cazul in care nu exista) in fisierul de log 
+//data la care se intampla o actiune(adaugare,view etc) si stabileste si ce actiune se intampla
 {
     int fis_log=open(path, O_CREAT|O_APPEND|O_WRONLY,0644);//USER rw, group & others doar r e de preferat la log 
     if(fis_log==-1)
@@ -78,8 +101,6 @@ void loggin_action(const char *action,char *path)
     }
 
     close(fis_log);
-
-
 
 }
 
@@ -157,6 +178,134 @@ void add_treasure(char *hunt,char logg_path[1024])
 
     
 }
+
+void list(char *hunt,char logg_path[1024])
+{
+    //verificam daca se poate deschide directorul
+    DIR *dir=NULL;
+    dir=opendir(hunt);
+
+    if(dir==NULL)
+    {
+        perror("Hunt doesn't exist!");
+        exit(-1);
+    }    
+
+//creeam pathul ex HUNT001/treasure_data
+    char path[64];
+    snprintf(path,sizeof(path),"%s/treasure_data",hunt);
+//vedem daca putem deschide fisierul doar read-only
+    int f=open(path,O_RDONLY,0777);
+    if(f==-1)
+    {
+        perror("Failed at openning!");
+        exit(-1);
+    }
+//folosim stat sa putem luam informatii despre director adica nume,nr bytes,ultima modificare 
+    struct stat fis_date;
+    if(stat(path,&fis_date)==-1)
+    {
+        perror("CAN'T GET INFO FROM FILE! ");
+        close(f);
+        closedir(dir);
+        exit(-1);       
+    }
+
+    printf("Nume hunt: %s\n",hunt);
+    printf("Dimensiune hunt: %ld\n",fis_date.st_size);
+    printf("Last modification: %s\n",ctime(&fis_date.st_atim.tv_sec));//timpul exact al utlimei modificari
+
+ 
+    printf("File content: \n\n");
+
+    TREASURE *t_buf=malloc(sizeof(TREASURE));
+    if(t_buf==NULL)
+    {
+        perror("NOT ENOUGH SPACE");
+        free(t_buf);
+        exit(-1);
+    }
+    int nr=1;
+
+    while (read(f, t_buf, sizeof(TREASURE)))// cat timp pot citi o comoara din treasure_data se executa 
+    {
+        printf("Treasure %d\n", nr++);
+        printf("ID: %d | User: %s | Longitude: %f | Latitude: %f | Clue: %s | Value: %d\n",
+               t_buf->id, t_buf->user, t_buf->longitude, t_buf->latitude, t_buf->clue, t_buf->value);
+    }
+
+    char message[30];
+//snprintf creeaza un sir formmatat intr-un buffer (adica intr-un string)
+//e n=mai sigur pentru ca nu se poate intampla overflow
+    snprintf(message,sizeof(message),"Listed %s",hunt);
+
+    loggin_action(message,logg_path);
+
+    free(t_buf);
+    close(f);
+    closedir(dir);
+}
+
+
+void remove_hunt(char *hunt,char logg_path[1024])
+{
+/*
+    prima data deschid si verific daca pot deschide directorul
+    dupa creez pathul catre fisierul treasure_data 
+    si symlink 
+
+    dupa incerc sa sterg pe rand fisierul treasure,dau remove la symlink,sterg fisierul log si in final directorul
+*/
+
+
+    DIR *dir=NULL;
+    dir=opendir(hunt);
+    if(dir==NULL)
+    {
+        perror("Doesn't exist :))");
+        exit(-1);
+    }
+
+    char path[64];
+    snprintf(path,sizeof(path),"%s/treasure_data",hunt);
+
+    char symlink[64];
+    snprintf(symlink,sizeof(symlink),"logged_hunt-%s",hunt);
+
+    if(remove(path)==0)
+    {
+        printf("Treasure file deleted!\n");
+    }
+    else
+    {
+        printf("It didn't removed the file !\n");
+
+    }
+
+    if(unlink(symlink)==0)
+    {
+        printf("Symbolic link removed with succes!\n");
+    }
+    else{
+        printf("Something happend and it didn't removed the symbolic link!\n");
+
+    }
+    if(remove(logg_path)==0)
+    {
+        printf("File loggin deleted!\n");
+    }
+    else{
+        printf("Failed to delete loggin file !\n");
+    }
+    if(rmdir(hunt)==0)
+    {
+        printf("Hunt succesfully deleted!\n");
+    }
+    else{
+        printf("Couldn't delete the HUNT!\n");
+    }
+
+}
 int main(int argc, char ** argv)
 {
     
@@ -169,13 +318,13 @@ int main(int argc, char ** argv)
             {
                 add_treasure(argv[2],log_path);
             }
-        else if(strcmp(argv[1],"--list"))
+        else if(strcmp(argv[1],"--list")==0)
         {
-               // list(...)   
+             list(argv[2],log_path);
         }
-        else if(strcmp(argv[1],"--remove_hunt"))
+        else if(strcmp(argv[1],"--remove_hunt")==0)
         {
-            //remove_hunt(...)
+            remove_hunt(argv[2],log_path);
         }
         else
             {
